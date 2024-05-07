573765845b340df55698d6f81e15b37ad6b77f1e
rorense <rkorense@gmail.com>
2024-05-08 09:47:37 +1200

    Added landing page skeleton

---
+++ duo/app/(marketing)/header.tsx
@@ -0,0 +1,3 @@
+export const Header = () => {
+  return <div className="border-b-2">Header</div>;
+};
---
+++ duo/app/(marketing)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="p-4 space-y-4 flex flex-col max-w-[200px] border">
+      This is a landing page
+    </div>
+  );
+}
